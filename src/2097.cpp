a6bb2776a39094088565e61fed9203dbf9d1fc07
nomatterwhoisthat <116099741+nomatterwhoisthat@users.noreply.github.com>
2025-04-21 02:13:55 +0300

    Update 2097.cpp

--- src/2097.cpp
+++ src/2097.cpp
@@ -6,16 +6,7 @@
 
 class Solution {
 public:
-    //void dfs(int node, std::unordered_map<int, std::deque<int>>& graph, std::vector<std::vector<int>>& result) {
-    //    while (!graph[node].empty()) {
-    //        int neighbour = graph[node].front();
-    //        graph[node].pop_front();
-    //        dfs(neighbour, graph, result);
-    //        result.push_back({ node, neighbour });
-    //    }
-    //}
-
-    std::vector<std::vector<int>> validArrangement(std::vector<std::vector<int>>& pairs) {
+ std::vector<std::vector<int>> validArrangement(std::vector<std::vector<int>>& pairs) {
         std::unordered_map<int, std::deque<int>> graph;
         std::unordered_map<int, int> in_degree, out_degree;
 
@@ -36,11 +27,6 @@ public:
             }
         }
 
-        //std::vector<std::vector<int>> result;
-        //dfs(start_node, graph, result);
-        //std::reverse(result.begin(), result.end());
-        //return result;
-
         std::vector<std::vector<int>> result;
         std::stack<int> stk;
         stk.push(start_node);
@@ -54,7 +40,9 @@ public:
             }
             else {
                 stk.pop();
-                result.push_back({stk.top(), node});
+                if(!stk.empty()) {
+                    result.push_back({stk.top(), node});
+                }
             }
         }
 
